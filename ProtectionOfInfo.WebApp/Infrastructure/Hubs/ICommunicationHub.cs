e1faa379fb78694557b73e6277b96ee874d9affa
lana-fairymay <72757886+lana-fairymay@users.noreply.github.com>
2022-10-18 22:14:59 +0300

    Реализация мессенджера

---
+++ ProtectionOfInfo.WebApp/Infrastructure/Hubs/ICommunicationHub.cs
@@ -0,0 +1,17 @@
+﻿using ProtectionOfInfo.WebApp.Data.Entities.ChatEntities;
+using ProtectionOfInfo.WebApp.ViewModels.ChatViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProtectionOfInfo.WebApp.Hubs
+{
+    public interface ICommunicationHub
+    {
+        Task SendMessageAsync(string userName, string message);
+        Task UpdateUsersAsync(IEnumerable<string> users);
+        Task SendImageAsync(string userName, string imageUrl);
+        Task SendUrlAsync(string userName, string url, string fileName);
+        Task SendErrorAsync(string errorMessage);
+        Task SendAllMessagesAsync(List<MessageViewModel> messages);
+    }
+}
