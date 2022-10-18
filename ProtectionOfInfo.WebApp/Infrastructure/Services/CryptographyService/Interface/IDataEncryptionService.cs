cb377a9ac623c5f5f1a17ee80da0b8ed1c9145f8
Alexandr <73179222+aggink@users.noreply.github.com>
2022-10-19 00:00:00 +0300

    Основная логика работы программы

---
+++ ProtectionOfInfo.WebApp/Infrastructure/Services/CryptographyService/Interface/IDataEncryptionService.cs
@@ -0,0 +1,10 @@
+﻿namespace ProtectionOfInfo.WebApp.Infrastructure.Services.CryptographyService
+{
+    public interface IDataEncryptionService
+    {
+        bool CheckPassword(string hashPassword);
+        string HashPassword(string password);
+        string Encrypt_Aes(string plainText);
+        string? Decrypt_Aes(string _cipherText, string hashpassword);
+    }
+}
