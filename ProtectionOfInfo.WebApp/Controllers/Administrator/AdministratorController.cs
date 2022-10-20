200c878dd38126532af5a7a93e7d794320d99470
TimGvr <114599462+TimGvr@users.noreply.github.com>
2022-10-20 12:11:24 +0300

    Add files via upload

---
+++ ProtectionOfInfo.WebApp/Controllers/Administrator/AdministratorController.cs
@@ -0,0 +1,302 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProtectionOfInfo.WebApp.Data;
+using ProtectionOfInfo.WebApp.Data.Entities;
+using ProtectionOfInfo.WebApp.Infrastructure.OperationResults;
+using ProtectionOfInfo.WebApp.Infrastructure.Services.PasswordValidatorsService;
+using ProtectionOfInfo.WebApp.Infrastructure.Services.PortInfoService;
+using ProtectionOfInfo.WebApp.ViewModels.AccountViewModels;
+using ProtectionOfInfo.WebApp.ViewModels.PortInfoViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProtectionOfInfo.WebApp.Controllers.Administrator
+{
+    [Authorize(Roles = AppData.AdministratorRoleName)]
+    [AutoValidateAntiforgeryToken]
+    public class AdministratorController : Controller
+    {
+        private readonly UserManager<MyIdentityUser> _userManager;
+        private readonly IMapper _mapper;
+        private readonly IMyPasswordValidatorService _passwordMyValidator;
+        private readonly IPortInfoService _portInfoService;
+
+        public AdministratorController(
+            UserManager<MyIdentityUser> userManager, 
+            IMapper mapper, 
+            IMyPasswordValidatorService myPasswordValidator,
+            IPortInfoService portInfoService)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+            _passwordMyValidator = myPasswordValidator;
+            _portInfoService = portInfoService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            if (User.Identity!.IsAuthenticated is false)
+            {
+                return View("Error", "Срок ожидания пользователя истек!");
+            }
+
+            SetLayOut(2);
+            return View();
+        }
+
+        #region Register
+
+        [HttpGet]
+        public IActionResult RegisterUser()
+        {
+            if (User.Identity!.IsAuthenticated is false)
+            {
+                return View("Error", "Срок ожидания пользователя истек!");
+            }
+
+            SetLayOut();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser(RegisterViewModel model)
+        {
+            if (User.Identity!.IsAuthenticated is false)
+            {
+                return View("Error", "Срок ожидания пользователя истек!");
+            }
+
+            SetLayOut();
+
+            if (ModelState.IsValid)
+            {
+                var isEnable = await _userManager.FindByNameAsync(model.UserName);
+                if(isEnable is not null)
+                {
+                    ModelState.AddModelError("", $"Пользователь с именем {model.UserName} уже существует!");
+                    return View(model);
+                }
+
+                if (model.PasswordValidation)
+                {
+                    var passwordValid = _passwordMyValidator.Validate(model.Password!);
+                    if (!passwordValid.Succeeded)
+                    {
+                        foreach (var error in passwordValid.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+
+                        return View(model);
+                    }
+                }
+
+                MyIdentityUser user = new MyIdentityUser
+                {
+                    UserName = model.UserName,
+                    BlockedUser = model.BlockedUser,
+                    PasswordValidation = model.PasswordValidation
+                };
+
+                var resultPassword = await _userManager.CreateAsync(user, model.Password);
+                if (!resultPassword.Succeeded)
+                {
+                    foreach (var error in resultPassword.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+
+                    return View(model);
+                }
+
+                var resultRole = await _userManager.AddToRoleAsync(user, AppData.UserRoleName);
+                if (!resultRole.Succeeded)
+                {
+                    foreach (var error in resultRole.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+
+                    return View(model);
+                }
+
+                SetLayOut(1);
+                return RedirectToAction("AllUsers", "Administrator");
+            }
+
+            return View(model);
+        }
+
+        #endregion
+
+        #region UpdateUser
+        [HttpGet]
+        public IActionResult AllUsers()
+        {
+            if (User.Identity!.IsAuthenticated is false)
+            {
+                return View("Error", "Срок ожидания пользователя истек!");
+            }
+
+            var allUsers = _userManager.Users.ToList();
+            var Users = _mapper.Map<List<UpdateUserForAdminViewModel>>(allUsers);
+
+            SetLayOut(1);
+            return View(Users);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateUser(string userId)
+        {
+            if (User.Identity!.IsAuthenticated is false)
+            {
+                return View("Error", "Срок ожидания пользователя истек!");
+            }
+
+            SetLayOut();
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(userId);
+                if (user is null)
+                {
+                    ModelState.AddModelError("", $"Пользователь c Id: { userId }  не найден!");
+                    return View();
+                }
+
+                return View(_mapper.Map<UpdateUserForAdminViewModel>(user));
+            }
+
+            SetLayOut(1);
+            return RedirectToAction("AllUsers", "Administrator");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateUser(UpdateUserForAdminViewModel modelUser)
+        {
+            if (User.Identity!.IsAuthenticated is false)
+            {
+                return View("Error", "Срок ожидания пользователя истек!");
+            }
+
+            SetLayOut();
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(modelUser.Id);
+                if(user == null)
+                {
+                    ModelState.AddModelError("", $"Пользователь { modelUser.UserName } (Id: { modelUser.Id } ) не найден!");
+                    return View(modelUser);
+                }
+
+                if (await _userManager.IsInRoleAsync(user, AppData.AdministratorRoleName) && modelUser.BlockedUser)
+                {
+                    ModelState.AddModelError("", $"Учетную запись администратора заблокировать нельзя!");
+                    return View(modelUser);
+                }
+
+                // изменение записи пользователя
+                user.BlockedUser = modelUser.BlockedUser;
+                user.PasswordValidation = modelUser.PasswordValidation;
+
+                var result = await _userManager.UpdateAsync(user);
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+
+                    return View(modelUser);
+                }
+
+                SetLayOut(1);
+                return RedirectToAction("AllUsers", "Administrator");
+            }
+
+            return View(modelUser);
+        }
+
+        #endregion
+
+        #region GetInfoPorts
+        [HttpGet]
+        public IActionResult GetInfoPorts()
+        {
+            var operation = OperationResult.CreateResult<List<PortInfoViewModel>>();
+
+            var result = _portInfoService.GetActiveTcpConnections();
+            if(result == null || result.Count() == 0)
+            {
+                operation.AddInfo("Активных подключений TCP не обнаружено");
+                result = new List<PortInfoViewModel>();
+            }
+            else
+            {
+                operation.AddSuccess("Список активных TCP подключений получен");
+                operation.Result = result;
+            }
+            
+            return View(operation);
+        }
+
+        [HttpGet]
+        public IActionResult GetInfoActiveTCPListeners()
+        {
+            ViewData["Title"] = "Активные прослушиватели TCP";
+            ViewData["MiniTitle"] = "Список активных прослушивателей TCP";
+            ViewData["ErrorTitle"] = ViewData["MiniTitle"];
+
+            var operation = OperationResult.CreateResult<List<EndPointViewModel>>();
+
+            var result = _portInfoService.GetActiveTcpListeners();
+            if (result == null || result.Count() == 0)
+            {
+                operation.AddInfo("Активных прослушивателей TCP не обнаружено");
+                result = new List<EndPointViewModel>();
+            }
+            else
+            {
+                operation.AddSuccess("Список активных активных прослушивателей TCP получен");
+                operation.Result = result;
+            }
+
+            return View("GetInfoActiveListeners", operation);
+        }
+
+        [HttpGet]
+        public IActionResult GetInfoActiveUDPListeners()
+        {
+            ViewData["Title"] = "Активные прослушиватели UDP";
+            ViewData["MiniTitle"] = "Список активных прослушивателей UDP";
+            ViewData["ErrorTitle"] = ViewData["MiniTitle"];
+
+            var operation = OperationResult.CreateResult<List<EndPointViewModel>>();
+
+            var result = _portInfoService.GetActiveUdpListeners();
+            if (result == null || result.Count() == 0)
+            {
+                operation.AddInfo("Активных прослушивателей UDP не обнаружено");
+                result = new List<EndPointViewModel>();
+            }
+            else
+            {
+                operation.AddSuccess("Список активных активных прослушивателей UDP получен");
+                operation.Result = result;
+            }
+
+            return View("GetInfoActiveListeners", operation);
+        }
+
+        #endregion
+
+        private void SetLayOut(int NavBar = 0)
+        {
+            ViewBag.Admin = true;
+            ViewBag.NavBar = NavBar;
+        }
+    }
+}
---
+++ ProtectionOfInfo.WebApp/Controllers/FileEncryptionController.cs
@@ -0,0 +1,272 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProtectionOfInfo.WebApp.Infrastructure.OperationResults;
+using ProtectionOfInfo.WebApp.Infrastructure.Services.CryptographyService.Interface;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ProtectionOfInfo.WebApp.Controllers.User
+{
+    /// <summary>
+    ///  // aggink: update summary - 27.02.2022 17:03:07
+    /// </summary>
+    [AllowAnonymous]
+    [AutoValidateAntiforgeryToken]
+    public class FileEncryptionController : Controller
+    {
+        private readonly ICryptographyService _cryptographyService;
+        private readonly ISettingEDSFileService _settingEDSFileService;
+        public FileEncryptionController(ICryptographyService cryptographyService, ISettingEDSFileService settingEDSFileService)
+        {
+            _cryptographyService = cryptographyService;
+            _settingEDSFileService = settingEDSFileService;
+        }
+
+        [HttpGet]
+        public IActionResult FileEncryption()
+        {
+            return View();
+        }
+
+        #region Encrypt/DeDecrypt File
+
+        [HttpPost]
+        public async Task<IActionResult> EncryptFile(IFormFile uploadedFile, string algorithm, string password)
+        {
+            var operation = OperationResult.CreateResult<FileContentResult>();
+
+            if (CheckValidEncrypt(operation, uploadedFile, password, algorithm))
+            {
+                return View(nameof(FileEncryption), operation);
+            }
+
+            try
+            {
+                string fileName = uploadedFile!.FileName;
+                string ext = Path.GetExtension(fileName);
+                string fileType = "application/" + ext.Trim('.');
+                fileName = "EncryptFile" + ext;
+
+                byte[] data = new byte[uploadedFile.Length];
+                using (var stream = uploadedFile.OpenReadStream())
+                {
+                    await stream.ReadAsync(data);
+                }
+                
+                var encrypt = await Task.Run(() => _cryptographyService.Encrypt(data, algorithm!, password));
+                if (encrypt.Length < data.Length)
+                {
+                    operation.AddError("В процессе шифрования файла произошла ошибка");
+                    return View(nameof(FileEncryption), operation);
+                }
+
+                return File(encrypt, fileType, fileName);
+            }
+            catch
+            {
+                operation.AddError("В процессе шифрования файла произошла ошибка");
+                return View(nameof(FileEncryption), operation);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DecryptFile(IFormFile uploadedFile, string password)
+        {
+            var operation = OperationResult.CreateResult<FileContentResult>();
+
+            if (CheckValidEncrypt(operation, uploadedFile, password, ""))
+            {
+                return View(nameof(FileEncryption), operation);
+            }
+
+            try
+            {
+                string fileName = uploadedFile!.FileName;
+                string ext = Path.GetExtension(fileName);
+                string fileType = "application/" + ext.Trim('.');
+                fileName = "DecryptFile" + ext;
+
+                byte[] data = new byte[uploadedFile.Length];
+                using (var stream = uploadedFile.OpenReadStream())
+                {
+                    await stream.ReadAsync(data);
+                }
+
+                var encrypt = await Task.Run(() => _cryptographyService.Decrypt(data, password));
+                if(encrypt.Length == 0)
+                {
+                    operation.AddError("В процессе расшифровки файла произошла ошибка");
+                    return View(nameof(FileEncryption), operation);
+                }
+
+                return File(encrypt, fileType, fileName);
+            }
+            catch
+            {
+                operation.AddError("В процессе расшифровки файла произошла ошибка");
+                return View(nameof(FileEncryption), operation);
+            }
+        }
+
+        private bool CheckValidEncrypt(OperationResult<FileContentResult> operation, IFormFile uploadedFile, string password, string algorithm)
+        {
+            bool error = false;
+
+            if (uploadedFile == null)
+            {
+                operation.AddError("Файл не задан");
+                error = true;
+            }
+
+            if (algorithm == null)
+            {
+                operation.AddError("Алгоритм шифрования не задан");
+                error = true;
+            }
+
+            if (string.IsNullOrEmpty(password))
+            {
+                operation.AddError("Пароль не задан");
+                error = true;
+            }
+
+            if (!ModelState.IsValid)
+            {
+                operation.AddError("Ошибка в обработке входных данных");
+                error = true;
+            }
+
+            return error;
+        }
+
+        #endregion
+
+        #region SignFiles 
+
+        public IActionResult DigitalSignature()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> CreateСertificate(int months = 3)
+        {
+            var operation = OperationResult.CreateResult<bool>();
+            if(!ModelState.IsValid)
+            {
+                operation.AddError("Ошибка при обработке введенных данных");
+                return View(nameof(DigitalSignature), operation);
+            }
+
+            var archive = await _settingEDSFileService.CreateСertificateAsync(months);
+            if(archive == null)
+            {
+                operation.AddError("При создании сертификата произошла ошибка");
+                return View(nameof(DigitalSignature), operation);
+            }
+
+            return File(archive.File, $"application/{archive.Extension}", archive.FileName);
+        }
+
+        public async Task<IActionResult> SignFile(IFormFile fileForSign, IFormFile privateKey, string passwordForSign)
+        {
+            var operation = CheckValidSign(fileForSign, privateKey, passwordForSign);
+            if (!operation.Ok)
+            {
+                return View(nameof(DigitalSignature), operation);
+            }
+
+            var signFile = await _settingEDSFileService.SignFileAsync(fileForSign, privateKey, passwordForSign);
+            if(signFile == null)
+            {
+                operation.AddError("При подписании файла произошла ошибка");
+                return View(nameof(DigitalSignature), operation);
+            }
+
+            return File(signFile.File, $"application/{signFile.Extension}", signFile.FileName);
+        }
+
+        public async Task<IActionResult> CheckSignFile(IFormFile fileForCheck, IFormFile publicKey, string passwordForCheck)
+        {
+            var operation = CheckValidSign(fileForCheck, publicKey, passwordForCheck);
+            if (!operation.Ok)
+            {
+                return View(nameof(DigitalSignature), operation);
+            }
+
+            var checkSign = await _settingEDSFileService.CheckSignFileAsync(fileForCheck, publicKey, passwordForCheck);
+            if(checkSign)
+            {
+                operation.AddSuccess("Файл прошел проверку ЭЦП");
+            }
+            else
+            {
+                operation.AddError("Файл не прошел проверку ЭЦП");
+            }
+
+            return View(nameof(DigitalSignature), operation);
+        }
+
+        public async Task<IActionResult> GetOriginalFile(IFormFile fileForDelete)
+        {
+            var operation = OperationResult.CreateResult<bool>();
+            if (!ModelState.IsValid)
+            {
+                operation.AddError("Ошибка при обработке введенных данных");
+                return View(nameof(DigitalSignature), operation);
+            }
+
+            if (fileForDelete == null)
+            {
+                operation.AddError("Файл для подписания не найден");
+                return View(nameof(DigitalSignature), operation);
+            }
+
+            var originFile = await _settingEDSFileService.GetOriginalFileAsync(fileForDelete);
+            if (originFile == null)
+            {
+                operation.AddError("При преобразовании файла к исходному виду произошла ошибка");
+                operation.Result = false;
+                return View(nameof(DigitalSignature), operation);
+            }
+
+            return File(originFile.File, $"application/{originFile.Extension}", originFile.FileName);
+        }
+
+        private OperationResult<bool> CheckValidSign(IFormFile file, IFormFile key, string password)
+        {
+            var operation = OperationResult.CreateResult<bool>();
+            operation.Result = true;
+
+            if (!ModelState.IsValid)
+            {
+                operation.AddError("Ошибка при обработке введенных данных");
+                operation.Result = false;
+            }
+
+            if (file == null)
+            {
+                operation.AddError("Файл для подписания не найден");
+                operation.Result = false;
+            }
+
+            if (key == null)
+            {
+                operation.AddError("Файл с приватным ключом не найден");
+                operation.Result = false;
+            }
+
+            if (String.IsNullOrEmpty(password))
+            {
+                operation.AddError("Пароль не задан");
+                operation.Result = false;
+            }
+
+            return operation;
+        }
+
+        #endregion
+    }
+}
